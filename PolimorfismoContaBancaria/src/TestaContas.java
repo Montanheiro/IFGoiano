25feac8efc549462c0fe26ad7133a95ffd90ae49
Lucas Montanheiro <lucasmontanheiro10@gmail.com>
2016-05-18 22:36:47 -0300

    Sem conta no principal

--- PolimorfismoContaBancaria/src/TestaContas.java
+++ PolimorfismoContaBancaria/src/TestaContas.java
@@ -2,21 +2,17 @@
 public class TestaContas {
 
 	public static void main(String[] args) {
-		Conta c = new Conta();
 		ContaCorrente cc = new ContaCorrente();
 		ContaPoupanca cp = new ContaPoupanca();
 		AplicacaoProgramada ap = new AplicacaoProgramada();
 		
-		c.deposita(2000);
 		cc.deposita(2000);
 		cp.deposita(2000);
-		ap.deposita(2000);
-		c.atualiza(0.01); 
+		ap.deposita(2000); 
 		cc.atualiza(0.01);
 		cp.atualiza(0.01);
 		ap.atualiza(0.01);
 		
-		c.saca(1000, 0.01);
 		cc.saca(1000, 0.01);
 		cp.saca(1000, 0.01);
 		ap.saca(1000, 0.01);
